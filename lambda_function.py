eb6365f7c9a361f2a80cfa6a5f0844679361a841
jroach <johnaroach@gmail.com>
2022-03-25 09:27:44 -0500

    Made more changes to print statement

--- lambda_function.py
+++ lambda_function.py
@@ -4,4 +4,6 @@ print('loading function')
 
 
 def lambda_handler(event, context):
+    print('#########################################')
     print('##########   HELLO WORLD !!!   ##########')
+    print('#########################################')
